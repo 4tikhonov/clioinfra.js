e352661ed2aefa32f4422def305a6004feb6ee58
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-09-22 14:00:30 +0200

    Path for modules added

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
 import sys
-sys.path.append('/home/strikes/clioinfra/modules')
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules')))
 from config import configuration, dataverse2indicators, load_dataverse, findpid, load_metadata, pidfrompanel
 
 dataset = 'hdl:10622/XUH7BG:65:66'
 dataset = "hdl:10622/I0YK5M:111:112"
+dataset = "hdl:10622/4X6NCK"
+
+if dataset:
+    (handle, revid, cliopid) = findpid(dataset)
+    print handle
+
 (metadata, pid, fileid, clio) = load_metadata(dataset)
 print pid
 for item in metadata:
