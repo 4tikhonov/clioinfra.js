aa04f270086f64c1a79be276dbaeec4d4d1cc1ef
¨Vyacheslav <4tikhonov@gmail.com>
2015-09-23 09:53:56 +0200

    Unit test for historical names added

--- tests/boundaries-test.py
+++ tests/boundaries-test.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import os
 import sys
+import unittest
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules')))
 from boundaries import getboundaries
+from config import configuration
 
-root = 'http://clearance.sandbox.socialhistoryservices.org'
-dataset = 'dataframe66_'
-jsondataset = getboundaries(root, dataset)
-print jsondataset
+class TestStringMethods(unittest.TestCase):
+
+  def test_boundaries(self):
+	config = configuration()
+	root = config['apiroot']
+	dataset = 'dataframe66_'
+	countries = getboundaries(root, dataset)
+	self.assertEqual(countries[1879][0], 'Austria-Hungary')
+
+if __name__ == '__main__':
+    unittest.main()
