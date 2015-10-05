7c5969784dd68d2126470a2907b4fb846bf0c2bf
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-10-05 11:09:24 +0200

    Print module with extra params

--- modules/chartprint.py
+++ modules/chartprint.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 
+import re
 from config import configuration
 from subprocess import Popen, PIPE, STDOUT
 
 def chartonprint(webpage, fileformat, year, code): 
     # Print function
-    (fileonweb) = ('')
+    (cmd, size, fileonweb) = ('', '1024', '')
 
     # Configuration
     config = configuration()
     path = config['path']
     phantompath = config['phantompath']
     imagepathloc = config['imagepath'] 
+    filesvg = config['tmpdir'] + '/' + year + '_' + code + '.svg'  
 
     if fileformat == 'shapefile':
         year = year
     else:
-        cmd = phantompath + "/phantomjs/lib/phantom/bin/phantomjs '" + path + "/static/renderHTML.js '" + webpage
+        cmd = phantompath + "/phantomjs/lib/phantom/bin/phantomjs " + path + "/static/renderHTML.js '" + webpage + "'"
 
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
         html = p.communicate()[0]
@@ -36,5 +38,10 @@ def chartonprint(webpage, fileformat, year, code):
         outdirfile = imagepathloc + '/' + outfile
         cmd = "/usr/bin/inkscape " + filesvg + " -e " + outdirfile + " -h " + size + " -D -b '#ffffff'"
         fileonweb = '/get?image=' + outfile
+
+    if cmd:
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        result = p.communicate()[0]
+        image = outfile
 
     return fileonweb
