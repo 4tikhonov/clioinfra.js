55b16ea0272bf9d780409790fe76f68cc4971063
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-10-05 15:20:21 +0200

    New panel test added with metadata

--- tests/panel_test.py
+++ tests/panel_test.py
@@ -6,22 +6,26 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules')))
 from tabulardata import loadcodes, load_api_data, countryset, json_dict, createframe, combinedata, data2panel
-from config import configuration, dataverse2indicators, load_dataverse, findpid, load_metadata
+from config import configuration, dataverse2indicators, load_dataverse, findpid, load_metadata, load_fullmetadata
 from historical import load_historical, histo
 from data2excel import panel2excel
 
 hist = {}
 handles = ["4X6NCK", "F16UDU","ZWRBOY"]
 customyear = ''
-fromyear = '1500'
+fromyear = '1990'
 toyear = '2000'
 customcountrycodes = '380,250,276,804,174,108,232,528,756'
 logflag = 0
 thisdir = '.'
+fullmetadata = {}
+pidslist = "4X6NCK,ZWRBOY"
+finaldir = '.'
+filename = 'test.xls'
 
 config = configuration()
 if fromyear:
-    historical = 1
+    historical = 0
     if historical:
         api = config['apiroot'] + "/collabs/static/data/historical.json"
         (regions, countries, ctr2reg) = histo(api)
@@ -29,4 +33,9 @@ if fromyear:
 
     (header, panelcells, codes, x1, x2, x3, x4) = data2panel(handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
     f = "panel.xlsx"
-    panel2excel(thisdir, f, header, panelcells)
+    if pidslist:
+        fullmetadata = load_fullmetadata(pidslist)
+    fullpath = panel2excel(finaldir, filename, header, panelcells, fullmetadata)
+    for year in panelcells:
+	print year
+    print x4
