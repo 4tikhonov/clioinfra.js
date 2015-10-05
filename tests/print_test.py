cedef21a966524306a613c2135a85d76f71a2f11
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-10-05 11:09:47 +0200

    Basic print test added

---
+++ tests/print_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '../modules')))
+from chartprint import chartonprint
+
+colors = '' #'greyscale'
+webpage = "http://dpe.sandbox.socialhistoryservices.org/collabs/static/v/worldmap.html?start=on&action=map&dataset=4X6NCK&catmax=6&ctrlist=&year=1981&lasty=2010&colors=" + colors
+fileformat = 'png'
+year = '1981'
+code = '4X6NCK'
+imagefile = chartonprint(webpage, fileformat, year, code)
+print imagefile
