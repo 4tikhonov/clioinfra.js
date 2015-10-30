ada13dcc8eff451e54ab980d995e7f3058d049d5
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-10-30 16:33:34 +0100

    Data validator added

--- modules/excel2data.py
+++ modules/excel2data.py
@@ -55,12 +55,68 @@ def parsehandle(handle):
         return ('', '', '')
 
 def downloadfile(root, path, fileID, key):
+    fullpath = path + str(fileID)
     if key:
-	fullpath = path + str(fileID)
-        cmd = "/usr/bin/wget -q \"" + root + "/api/access/datafile/" + str(fileID) + "?key=" + key + "&show_entity_ids=true&q=authorName:*\" -O " + fullpath + " --no-check-certificate"
+        cmd = "/usr/bin/wget -q \"" + root + "/api/access/datafile/" + str(fileID) + "?key=" + str(key) + "&show_entity_ids=true&q=authorName:*\" -O " + fullpath + " --no-check-certificate"
         p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
         response = p.stdout.read()
     return fullpath
+
+def excelvalidator(path, filename, resultfile, tmpdir):
+    # Convert to CSV
+    cmd = path + "/../modules/xlsx2csv.py " + filename + " > " + resultfile + ".tmp" 
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    response = p.stdout.read()
+
+    # Read as dataframe
+    file = open(resultfile + ".tmp", 'r')
+    contents = file.read()
+    strings = contents.split("\n")
+    columnlen = 0
+    for item in strings:
+        data = item.split('|')
+        if len(data) > columnlen:
+            columnlen = len(data)
+    matrix = []
+    for item in strings:
+        data = item.split('|')
+        dataset = []
+        thislen = len(data)
+        for i in range(0, columnlen):
+            try:
+                dataitem = data[i]
+                dataitem = dataitem.replace("\r", "")
+                dataset.append(dataitem)
+            except:
+                dataset.append('')
+        matrix.append(dataset)
+    df = pd.DataFrame(matrix)
+    df = df.fillna('')
+    info = df.head()
+    #df.convert_objects(convert_numeric=True)
+    data = df
+    # Columns, title and units
+    col = data.ix[2]
+    title = data.ix[0][0]
+    units = data.ix[1][0]
+    df.columns = col
+    df.to_csv(resultfile + '.csv', cols=col, sep='\t', encoding='utf-8')
+
+    # Save json
+    if col.any():
+        d = [
+        dict([
+        (colname, row[i])
+        for i,colname in enumerate(df.columns)
+        ])
+        for row in df.values
+        ]
+
+        jsondatafile = resultfile + '.json'
+        with open(jsondatafile, 'w') as filej:
+            filej.write(json.dumps(d))
+
+    return (jsondatafile, title, units)
 
 def dataextractor(fullpath, path, handle, fileID):
     dataset = {}
@@ -71,11 +127,11 @@ def dataextractor(fullpath, path, handle, fileID):
     fileID = handle
 
     for sheetname in sheets:
-        df = xl.parse(sheetname)
+	pd.options.display.float_format = '{:,.0f}'.format
+        df = xl.parse(sheetname) #, dtype=int)
 	df = df.fillna('')
         info = df.head()
         data = df
-	print info
     
         # Columns first
         col = data.columns
@@ -86,19 +142,24 @@ def dataextractor(fullpath, path, handle, fileID):
 
         if DEBUG:
             print sql
+	print str(path) + str(tablename) + '.csv'
         df.to_csv(path + tablename + '.csv', cols=col, sep='\t', encoding='utf-8')
         dataset[sheetname] = data
 
 	godata = {}
     	for i,colname in enumerate(df.columns):
             colname = col[i]
-            noname = re.match("Unnamed", colname)
-            if noname:
-                skip = i
-            else:
+	    try:
+		noname = re.match("Unnamed", colname)
+		noname = re.match("date", colname)
+                if noname:
+                    skip = i
+	    except:
                 godata[str(colname)] = i
     
         # Data insert
+	#df = df.drop('start date', 1)
+	#df = df.drop('end date', 1)
         d = [
         dict([
         (colname, row[i])
@@ -107,9 +168,11 @@ def dataextractor(fullpath, path, handle, fileID):
         for row in df.values
         ] 
     
+	jsondatafile = path + tablename + '.json'
         with open(path + tablename + '.json', 'w') as filej:
             filej.write(json.dumps(d))
         sheetid = sheetid + 1
+	return jsondatafile
 
     if DEBUG:
         for sheet in dataset:
