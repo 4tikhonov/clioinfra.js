0ceef279e3424abd399c6a1720ae76142b7ccf9a
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-11-24 16:07:03 +0100

    Test extended with storage connection to get csv dataframes

--- tests/csvreader-test.py
+++ tests/csvreader-test.py
@@ -8,7 +8,11 @@ import pandas as pd
 import numpy as np
 import random
 import vincent
+import sys
+import os
 from vincent import Axis, AxisProperties, PropertySet, ValueRef
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '../modules')))
+from storage import data2store, readdata, readdataset, readdatasets, datasetadd, formdatasetquery
 from pandas.io.json import json_normalize
 import re
 import openpyxl
@@ -17,7 +21,23 @@ import requests
 from StringIO import StringIO
 
 url = "http://dpe.sandbox.socialhistoryservices.org/api/datasets?handle=Panel[%27hdl:10622/MRPFJ0:243:244%27]&format=csv"
-res = requests.get(url)
-csvio = StringIO(res.content)
-dataframe = pd.read_csv(csvio, sep='\t', dtype='unicode')
-print dataframe.to_html
+onlinetest = ''
+
+if onlinetest:
+    res = requests.get(url)
+    csvio = StringIO(res.content)
+    dataframe = pd.read_csv(csvio, sep='\t', dtype='unicode')
+    print dataframe.to_html
+else:
+    handles = []
+    handle = "hdl:10622/MRPFJ0"
+    handles.append(handle)
+    hquery = formdatasetquery(handles,'')
+    print hquery
+    d = readdatasets('datasets', json.loads(hquery))
+    for x in d:
+        csvframe = StringIO(x['csvframe'])
+        dataframe = pd.read_csv(csvframe, sep='\t', dtype='unicode')
+        print dataframe.to_html
+
+
