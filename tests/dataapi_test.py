8f5e6dea52d6e33983f82a8e0b3a89e5e58f2fcd
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-11-25 11:52:19 +0100

    Test updated

--- tests/dataapi_test.py
+++ tests/dataapi_test.py
@@ -14,7 +14,7 @@ hist = {}
 
 # Test case
 handles = ["4X6NCK", "F16UDU","ZWRBOY"]
-handles = ["4X6NCK"]
+handles = ["hdl:10622/4X6NCK"]
 customyear = '1901'
 fromyear = '1500'
 toyear = '2000'
@@ -30,7 +30,9 @@ if fromyear:
         (regions, countries, ctr2reg) = histo(api)
         hist = countries
 
-    (header, panelcells, codes, x1, x2, x3, x4) = data2panel(handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
+# In: (handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
+    (header, panelcells, codes, x1, x2, x3, x4, x5) = data2panel(handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
+# Out: (header, panelcells, code2ctr, datahub, data, handle2ind, unit2ind, original)
 
 modern = moderncodes(config['modernnames'], config['apiroot'])
 jsondata = data2json(modern, codes, panelcells)
