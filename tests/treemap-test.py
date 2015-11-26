457a8c28289d05ac6f91fbc025e2e98c8d62e92b
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-11-26 16:19:20 +0100

    Treemap test added

---
+++ tests/treemap-test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import json
+import sys
+import os
+import simplejson
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '../modules')))
+from config import configuration, dataverse2indicators, load_dataverse, findpid, load_metadata
+from storage import data2store, readdata, readdataset, readdatasets, datasetadd, formdatasetquery
+from datasets import loaddataset, countrystats, loaddataset_fromurl, loadgeocoder
+from sys import argv
+import pandas as pd
+import numpy as np
+
+
+handles = []
+remote = 'on'
+handle = "hdl:10622/LIKXCZ"
+config = configuration()
+
+if remote:
+    (classification, dataset) = loaddataset_fromurl(config['apiroot'], handle)
+else:
+    dataset = loaddataset(handles)
+
+(modern, historical) = loadgeocoder(dataset)    
+handle = "hdl:10622/DIUBXI"
+handles = []
+handles.append(handle)
+if remote:
+    (class1, dataset) = loaddataset_fromurl(config['apiroot'], handle)
+else:
+    dataset = loaddataset(handles)
+
+#modern.ix[76]['country']
+#historical.ix[1]
+#dataset.ix[56]
+(df, result) = countrystats(dataset)
+for idc in result:
+    value = result[idc]    
+    try:
+        ctr = dataset.ix[idc]['Continent, Region, Country']
+        print "\t{ \"name\": \"" + str(ctr) + "\", \"size\": " + str(value) + "}, "
+    except:
+        skip = idc
