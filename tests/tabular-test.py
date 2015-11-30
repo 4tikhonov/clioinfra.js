520f3ef5153e247740eaaa62dbcd580b58448a7c
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-11-30 15:51:35 +0100

    Test to create datasets updated

--- tests/tabular-test.py
+++ tests/tabular-test.py
@@ -17,27 +17,32 @@ fromyear = '1500'
 toyear = '2000'
 customcountrycodes = '380,250,276,804,174,108,232,528,756'
 #customcountrycodes = '528,756'
-handles = ["GDAO0N"]
+handles = ["hdl:10622/GDAO0N"]
 logflag = 0
+config = configuration()
 
 if fromyear:
-    historical = 1
+    historical = 0
     if historical:
-        api = "http://clearance.sandbox.socialhistoryservices.org/collabs/static/data/historical.json"
-        (regions, countries, ctr2reg) = histo(api)
+        api = config['clearance'] + "/collabs/static/data/historical.json"
+        (regions, countries, ctr2reg, webmapper, geo) = histo(api, '')
         hist = countries
 
-    (header, panelcells, codes, datahub, data, handle2ind, unit2ind) = data2panel(handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
-    xdir = "/home/strikes/data"
+    (header, panelcells, codes, datahub, data, handle2ind, unit2ind, original) = data2panel(handles, customcountrycodes, fromyear, toyear, customyear, hist, logflag)
     f = "panel.xlsx"
-    panel2excel(xdir, f, header, panelcells)
+    #(handles, pidslist) = pidfrompanel(pid)
+    pidslist = handles
+    try:
+        if pidslist:
+            fullmetadata = load_fullmetadata(pidslist)
+    except:
+	fullmetadata = ''
+    # Create panel
+    panel2excel(config['tmpdir'], f, header, panelcells, fullmetadata)
 
-handles = ["4X6NCK"]
-handles = ["GDAO0N"]
+# Create individual files 
 for pid in handles:
-    datadir = '/home/strikes/data'
-    datadir = "/home/strikes/sik/web/collabs/static/data"
     testfile = 'test1.xls'    
     print data[pid]
-    individual_dataset(datadir, testfile, handle2ind[pid], unit2ind[pid], datahub, data[pid], codes)
+    individual_dataset(config['tmpdir'], testfile, handle2ind[pid], unit2ind[pid], datahub, data[pid], codes, fullmetadata)
 
