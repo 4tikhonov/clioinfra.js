fc248e68072b5c98196766955462f05f7193b3af
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-12-03 17:17:50 +0100

    Test updated

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -7,23 +7,24 @@ import simplejson
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '../modules')))
 from config import configuration, dataverse2indicators, load_dataverse, findpid, load_metadata
 from storage import data2store, readdata, readdataset, readdatasets, datasetadd, formdatasetquery
-from datasets import loaddataset, loaddataset_fromurl, loadgeocoder, treemap, selectint, buildgeocoder
+from datasets import loaddataset, loaddataset_fromurl, loadgeocoder, treemap, selectint, buildgeocoder, content2dataframe
 from sys import argv
 
 handles = []
-remote = 'on'
 config = configuration()
+config['remote'] = ''
 
 # Geocoder
 handle = config['geocoderhandle']
-if remote:
-    (classification, dataset) = loaddataset_fromurl(config, handle)
-else:
-    dataset = loaddataset(handles)
+(classification, dataset, title, units) = content2dataframe(config, config['geocoderhandle'])
+#if remote:
+#    (classification, dataset) = loaddataset_fromurl(config, handle)
+#else:
+#    dataset = loaddataset(handles)
 
-(geocoder, geolist, oecdlist) = buildgeocoder(dataset, config)
-print geocoder
-(modern, historical) = loadgeocoder(dataset, 'geocoder') 
+(geocoder, geolist, oecdlist) = buildgeocoder(dataset, config, '')
+#print geocoder
+(modern, historical) = loadgeocoder(config, dataset, 'geocoder') 
 
 switch = 'historical'
 switch = 'modern'
@@ -37,15 +38,16 @@ else:
 
 handle = "hdl:10622/DIUBXI"
 handle = "hdl:10622/WNGZ4A"
-#handle = "hdl:10622/GZ7O1K"
+handle = "hdl:10622/GZ7O1K"
 
-if remote:
-    (class1, dataset) = loaddataset_fromurl(config, handle)
-else:
-    dataset = loaddataset(handles)
+#if remote:
+#    (class1, dataset) = loaddataset_fromurl(config, handle)
+#else:
+#    dataset = loaddataset(handles)
+(class1, dataset, title, units) = content2dataframe(config, handle)
 
 (cfilter, notint) = selectint(activeindex.values)
-(moderndata, historicaldata) = loadgeocoder(dataset, '')
+(moderndata, historicaldata) = loadgeocoder(config, dataset, '')
 if switch == 'modern':
     maindata = moderndata
 else:
@@ -60,8 +62,8 @@ year = '2004'
 
 test = ''
 if test:
-    ccode = '1639'
-    year = '1945'
+#    ccode = '1639'
+#    year = '1945'
     x = dataset.ix[str(ccode)][str(year)] 
     dataf = dataset.ix[str(ccode)]
     print dataset.to_html
