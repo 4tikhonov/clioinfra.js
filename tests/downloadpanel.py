50df9b25e731c3b0540cef8cef4ee93e62da95fd
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-12-15 09:44:14 +0100

    Test of panel updated

--- tests/downloadpanel.py
+++ tests/downloadpanel.py
@@ -10,14 +10,18 @@ from data2excel import panel2excel
 config = configuration()
 if config:
     handles = ['hdl:10622/2XMLW5', 'hdl:10622/SMIOST']
-    handles = ['hdl:10622/4X6NCK'] #, 'hdl:10622/6FCHEF', 'hdl:10622/ZWRBOY']
+    handles = ['hdl:10622/4X6NCK', 'hdl:10622/CSNW0F'] #, 'hdl:10622/6FCHEF', 'hdl:10622/ZWRBOY']
     if handles[0]:
 	ctrlist = '1523,1525'
  	ctrlist = '528'
-	startyear = '1929'
+	startyear = '1950'
 	endyear = '1955'
+	datafilter = {}
+	datafilter['startyear'] = startyear
+	datafilter['endyear'] = endyear
+	datafilter['ctrlist'] = ctrlist
 	switch = 'modern'
-	(header, panelcells, metadata, totalpanel) = build_panel(config, switch, handles, startyear, endyear, ctrlist)
+	(header, panelcells, metadata, totalpanel) = build_panel(config, switch, handles, datafilter)
 	datadir = "/home/dpe/tmp"
 	filename = "paneltest.xlsx"
 	metadata = []
