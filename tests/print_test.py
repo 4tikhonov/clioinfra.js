7b5f8e93fac2b072c563e07b1ae6230c2184d792
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2015-12-16 13:51:42 +0100

    Print map test updated

--- tests/print_test.py
+++ tests/print_test.py
@@ -4,15 +4,17 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), '../modules')))
 from chartprint import chartonprint
+from config import configuration
 
-def print_map():
+def print_map(config):
     colors = ''# 'greyscale'
-    webpage = "http://dpe.sandbox.socialhistoryservices.org/collabs/static/v/worldmap.html?start=on&action=map&dataset=4X6NCK&catmax=6&ctrlist=&year=1981&lasty=2010&colors=" + colors
+    webpage = "http://localhost/collabs/static/v/worldmap.html?start=on&action=map&handle=hdl:10622/8FCYOX&catmax=6&ctrlist=&year=1981&lasty=2010&colors=" + colors
     fileformat = 'png'
     year = '1981'
     code = '4X6NCK'
-    imagefile = chartonprint(webpage, fileformat, year, code)
+    imagefile = chartonprint(webpage, fileformat, year, code, config['proxy'])
     return imagefile
 
-imagefile = print_map()
+config = configuration()
+imagefile = print_map(config)
 print imagefile
