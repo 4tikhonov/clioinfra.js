a140e59f06fc2f36189e3a1fc0ecf985d5f57992
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2016-01-12 13:50:27 +0100

    Settings added

--- cliocore/__init__.py
+++ cliocore/__init__.py
@@ -1,1 +1,5 @@
 from __future__ import absolute_import
+configpath = "/etc/apache2/clioinfra.conf"
+forbiddenuri = "(curl|bash|mail|ping|sleep|passwd|cat\s+|cp\s+|mv\s+|certificate|wget|usr|bin)"
+ERROR1 = "Something went wrong with special characters in url..."
+ERROR2 = "Something definitely went wrong with specific terms in url..."
