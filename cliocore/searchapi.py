af938500eafcbef801875b20c8b85fbde5150e7a
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2016-01-14 16:26:16 +0100

    Extra SearchAPI class added to get handles from HTML

---
+++ cliocore/searchapi.py
@@ -0,0 +1,112 @@
+import os
+import urllib2
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import BytesIO as StringIO
+
+import requests
+import re
+
+class ExtrasearchAPI:
+    def __init__(self, root, dataversename):
+        self.config = {}
+	self.dataverseroot = root
+	self.datasersename = dataversename
+	self.query = ''
+
+    # read all handles from HTML page
+    def read_dataverse_html_page(self, page):
+        query = ''
+        url = self.dataverseroot + "/dataverse/" + self.dataversename + "?types=datasets"
+        if int(page) > 1:
+            url+="&page=" + str(page)
+
+        response = urllib2.urlopen(url)
+        html = response.read()
+        pattern = re.compile(r'>http\:\/\/hdl.handle.net\/\d+\/(\S+)<', re.IGNORECASE)
+        match = pattern.findall(html)
+        for q in match:
+            query+=q + ' '
+        
+        total = re.search(r'of\s+(\d+)\s+Results', html)            
+        return (query, total.group(1))
+
+    def read_all_datasets(self):
+        condition = True
+        start = 0
+        page = 1
+        per_page = 10
+        tq = ''
+        while (condition):
+            (query, total) = self.read_dataverse_html_page(page)    
+            results = page * per_page    
+            tq+=tq + query
+            page+=1
+            if results > int(total):
+                condition = False
+	self.query = tq
+        return tq
+
+    def get_datasets_from_html(root, dataversename):
+        query = ''
+        url = root + "/dataverse/" + dataversename + "?types=datasets"
+        response = urllib2.urlopen(url)
+
+        html = response.read()
+        pattern = re.compile(r'>http\:\/\/hdl.handle.net\/\d+\/(\S+)<', re.IGNORECASE)
+        match = pattern.findall(html)
+        for q in match:
+            query+=q + ' '
+        return query
+
+    def search_by_handles(self, searchquery):
+        (searchhandles, metadata, pids) = ('', [], [])
+        if ' ' in searchquery['q']:
+    	    pids = searchquery['q'].split()
+        if ',' in searchquery['q']:
+            pids = searchquery['q'].split(",")
+
+        if not pids:
+            return metadata
+
+        for handle in pids:
+            ids = re.search(r'hdl\:\d+\/(\w+)', handle, re.M|re.I)
+            if ids:
+    	        identificator = ids.group(1)
+    	        searchhandles = searchhandles + identificator + ' '
+
+        if searchhandles:
+     	    searchquery['q'] = searchhandles
+    	    metadata = search_by_keyword(self, searchquery)
+
+        return metadata
+
+    def search_by_keyword(self, searchquery):
+        if searchquery:
+            url = '{0}/search'.format(
+                self.native_base_url
+            )
+
+     	options = ['q', 'type', 'subtree', 'sort', 'order', 'per_page', 'start', 'show_relevance', 'show_facets', 'fq']
+
+    	# Form parameters URI
+      	params = {'key': self.token}
+    	for opt in options:
+    	    if opt in searchquery:
+    		params[opt] = searchquery[opt]
+
+    	resp = requests.get(url, params)
+
+        if resp.status_code == 404:
+            raise self.VersionJsonNotFoundError(
+                'JSON metadata could not be found for this version.'
+            )
+        elif resp.status_code != 200:
+                raise self.ConnectionError('JSON metadata could not be retrieved.')
+
+        metadata = resp.json()['data']
+
+        return metadata
+
