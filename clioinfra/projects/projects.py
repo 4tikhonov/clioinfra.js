e8258ff826704d3daff727a6ebe84cacca4c1383
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2016-03-14 14:41:50 +0100

    Login/logout path adjusted

--- clioinfra/projects/projects.py
+++ clioinfra/projects/projects.py
@@ -86,19 +86,38 @@ def browse(settings=''):
 @app.route('/logout', methods=['GET'])
 def logout(settings=''):
     session['name'] = ''
-    return make_response(render_template('iish/login.html'))
+    try:
+        if session['project']:
+            sandboxflag = re.search("sandbox", request.url)
+            if sandboxflag:
+                projecturl = "%s/%s" % (clioinfra.config['apiroot'], session['project'])
+            else:
+                projecturl = "%s/%s" % (clioinfra.config['proxy'], session['project'])
+            return redirect(projecturl, code=302)
+    except:
+        return make_response(render_template('iish/login.html'))
 
 @app.route('/login', methods=['POST', 'GET'])
 def login(settings=''):
     clioinfra = Configuration()
     openldap = OpenLDAP()
+    if request.args.get('project'):
+	session['project'] = request.args.get('project')
     try:
         thisuser = openldap.authentificate(request.form.get('login'), request.form.get('password'))
 	if thisuser[0][1]['uid'][0]:
 	    session['name'] = thisuser[0][1]['displayName'][0]
 	    session['uid'] = thisuser[0][1]['uid'][0]
 	name = str(thisuser[0][1]['displayName'][0])
-	return make_response(render_template('iish/login.html', username=name))
+	if session['project']:
+	    sandboxflag = re.search("sandbox", request.url)
+	    if sandboxflag:
+		projecturl = "%s/%s" % (clioinfra.config['apiroot'], session['project'])
+	    else:
+	        projecturl = "%s/%s" % (clioinfra.config['proxy'], session['project'])
+	    return redirect(projecturl, code=302)
+	else:
+	    return make_response(render_template('iish/login.html', username=name))
     except:
         return make_response(render_template('iish/login.html'))
 
@@ -114,9 +133,6 @@ def projectpage(e):
     projectinfo = {}  
     data = {}
     try:
-	#projecthandle = Project('database', project)
-	#return 'x'
-	#return str(projecthandle.project)
         data = readdata('projects', 'uri', project)
 	# Remove data
         for item in data:
