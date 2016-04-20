1adea759645ebc40128ff3cf1001d433c4249d3e
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2016-04-20 13:25:51 +0200

    Compiler test updated

--- tests/compiler_test.py
+++ tests/compiler_test.py
@@ -16,15 +16,15 @@ datafilter = {}
 datafilter['startyear'] = '1500'
 datafilter['endyear'] = '2010'
 datafilter['ctrlist'] = ''
-datafilter['ctrlist'] = '528,14,18,67'
-handle = "hdl:10622/WNGZ4A"
+#datafilter['ctrlist'] = '528,14,18,67'
+handle = 'hdl:10622/SO62N5'
 switch = 'historical'
-fullpath = ''
+fullpath = '/home/dpe/tmp'
 
 a = datetime.now()
-(outfile, finalsubset) = dataframe_compiler(config, fullpath, handle, switch, datafilter)
+(filetitle, fullpath, finalsubset) = dataframe_compiler(config, fullpath, handle, switch, datafilter)
 b = datetime.now()
 d = b - a
 print "Time: " + str(d.seconds) + " seconds"
-print finalsubset.ix[67][1831]
+#print finalsubset.ix[67][1831]
 
