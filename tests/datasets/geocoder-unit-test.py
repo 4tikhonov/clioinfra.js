6509b06aa03242f450766d4cb5d8984f14146b11
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2016-05-04 11:50:46 +0200

    Geocoder unit test added

---
+++ tests/datasets/geocoder-unit-test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import
+import os
+import sys
+import unittest
+import json
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("__file__"), '../../')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("__file__"), '../../modules')))
+from cliocore.configutils import Configuration
+from cliocore.datasetmanager import Dataset
+from cliocore.geocoder import Geocoder
+
+class ConfigTestClass(unittest.TestCase):
+    def test_settings(self):
+        self.clioinfra = Dataset()
+	self.geo = Geocoder()
+	self.geo.buildgeocoder('')
+	self.assertTrue(self.geo.geonames)
+	clioindex = self.clioinfra.clioindex()
+        self.assertTrue(self.clioinfra.config['dataverseroot'])
+        self.assertTrue(self.clioinfra.config['apiroot'])
+        self.assertTrue(self.clioinfra.config['key'])
+
+if __name__ == '__main__':
+    unittest.main()
